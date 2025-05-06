00094fb062bdf53e91ea20da0326c950d0e69acc
schilling <jannik.schilling@uni-rostock.de>
2025-05-06 15:20:49 +0200

    first steps for config file!

---
+++ config_tools.py
@@ -0,0 +1,8 @@
+from qgis.PyQt import QtWidgets
+
+def get_config(json_file):
+    pass
+    
+class oswegeToolsConfigDialog(QtWidgets.QDialog):#, FORM_CLASS):
+    def __init__(self, json_file, parent=None):
+        pass
